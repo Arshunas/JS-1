29eaa9d568d803401227bb8bbb7788996d1eca26
Sarunas <sarunas.stankevicius@mailbox.org>
2023-01-19 20:20:39 +0200

    Boolean
    Signed-off-by: Sarunas <sarunas.stankevicius@mailbox.org>

---
+++ js/04-boolean.js
@@ -0,0 +1,17 @@
+/*
+BOOLEAN (logine reiksme)
+- true
+- false
+*/
+
+const tiesa = true;
+const melas = false;
+
+console.log(tiesa);
+console.log(melas);
+
+const arPilnametis = true;
+const arLyja = false;
+
+console.log(arPilnametis);
+console.log(arLyja);
